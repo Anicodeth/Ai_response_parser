80ae642c08cfdbcc6e475cef44e5cd7bfc42e3e0
Ananya <afmtrendaid@gmail.com>
2023-04-14 01:53:30 -0700

    Vercel

--- index.js
+++ index.js
@@ -236,7 +236,9 @@ app.post('/api/addperson',(req, res)=>{
 });
 
 //Main listening post
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(`${PORT}`, ()=>{
     console.log(`Running on port ${PORT}`)
 });
+
+module.exports = app;
